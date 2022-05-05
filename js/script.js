11c715f5ae98447d8225f00f2437c238fe4296c9
Madeleine Forgeron <99274168+MadeleineForgeron@users.noreply.github.com>
2022-05-05 22:11:26 +0000

    :>

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@
 /**
  * This function displays the discount depending on age and day of the week.
  */
+
 function displayCost() {
   	// initialize variables
 	let cost = "Please make a selection above."
@@ -17,16 +18,16 @@ function displayCost() {
 	let select = document.getElementById('day');
 	let day = select.options[select.selectedIndex].value;
 
-    	//  if age is less than 5 or more than 95 it is free
+      	//  if age is less than 5 or more than 95 it is free	
 	if (age < 5 || age > 95) {
 		cost = "It is free."
 	}
-      	// if age is equal or more then 12 or equal or less then 21 or if it is tuesday or thuraday then you get a student discount
-	else if ((day == "Tuesday") || (day == "Thursday")
-			 (age >=12) && (age <=21)) {
+          	// if age is equal or more then 12 or equal or less then 21 or if it is tuesday or thursday then you get a student discount
+	else if ((day == "Tuesday") || (day == "Thursday)") 
+			 || (age >=12) && (age <=21)) {
 		cost = "You get a student discount."
-	}
-      	// else you have to pay the regular price
+  }
+ 	// else you have to pay the regular price
 	else if ((age > 0) || (day != "")) {		
 		cost = "You have to pay regular price."
 	}
